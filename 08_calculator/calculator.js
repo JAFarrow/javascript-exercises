0ff6e7d855cc708b75b933a5e69d1b4b5c951e30
Justin <f.justin@tutanota.com>
2023-02-23 21:05:50 +0200

    Completed Project 8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(intOne, intTwo) {
+	return intOne += intTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(intOne, intTwo) {
+	return intOne -= intTwo;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce(function(a, b) {
+    return a + b;
+  }
+  , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(function(a, b) {
+    return a * b;
+  });
 };
 
-const power = function() {
-	
+const power = function(int, sqr) {
+  let returnVal = 1;
+  for (i = 0; i < sqr; i++) {
+    returnVal = returnVal * int;
+  }
+  return returnVal;
 };
 
-const factorial = function() {
-	
+const factorial = function(int) {
+  let fact = 1;
+  for (i = 0; i < int; i++) {
+    fact = fact * (i + 1);
+  }
+  return fact;
 };
 
 // Do not edit below this line
