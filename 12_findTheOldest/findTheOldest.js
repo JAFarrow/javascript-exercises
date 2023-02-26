cad74e024c28451ab99fb566867e6e9a29fa7cb5
Justin <f.justin@tutanota.com>
2023-02-26 21:35:04 +0200

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let workingArray = obj;
+    let nameAndAge = [];
+    for (i = 0; i < workingArray.length; i++) {
+        if (workingArray[i].yearOfDeath == null) {
+            nameAndAge[i] = {
+                "name": workingArray[i].name,
+                "age": (new Date().getFullYear() - workingArray[i].yearOfBirth),
+            }
+        } else {
+            nameAndAge[i] = {
+                "name": workingArray[i].name,
+                "age": (workingArray[i].yearOfDeath - workingArray[i].yearOfBirth),
+            }
+        }
+    }
 
+    let theOldest = nameAndAge.reduce(function(former, current) {
+        return former.age > current.age ? former : current;
+    });
+    return theOldest;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
